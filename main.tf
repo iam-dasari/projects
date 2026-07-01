5af3fd00d2516dc256de71352c4b6c4cd2504845
DasariBullaiah <dasaridevops2025@gmail.com>
2026-07-01 14:23:08 +0530

    EC2 Deployment

--- main.tf
+++ main.tf
@@ -1,33 +1,39 @@
-resource "aws_instance" "ec2" {
-    ami = "ami-0521cb2d60cfbb1a6"
-    instance_type = "t3.micro"
+resource "aws_security_group" "inline_sg" {
+  name        = "roboshop-sg"
+  description = "Managed entirely inline"
+  vpc_id      = var.vpc_id
 
-    security_groups = [aws_security_group.inline_sg.id]
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "terraform-demo-1"
-        Project = "Roboshop"
-        Environment = "dev"
-    }
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "roboshop-sg"
+    Project     = "Roboshop"
+    Environment = "dev"
+  }
 }
 
-resource "aws_security_group" "inline_sg" {
-    name        = "roboshop-sg"
-    description = "Managed entirely inline"
-    #vpc_id      = "vpc-0123456789abcdef0"
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0521cb2d60cfbb1a6"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.inline_sg.id]
 
-    ingress {
-        description = "Allow HTTPS"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  tags = {
+    Name        = "terraform-demo-1"
+    Project     = "Roboshop"
+    Environment = "dev"
+  }
 }
